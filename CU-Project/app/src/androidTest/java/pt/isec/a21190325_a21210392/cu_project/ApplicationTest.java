1540cf76413ace59997b243813d0c6b3b2eba004
andregoncrod <andrerodrigues.pombal@gmail.com>
2015-04-23 22:34:45 +0100

    teste
    teste

---
+++ CU-Project/app/src/androidTest/java/pt/isec/a21190325_a21210392/cu_project/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pt.isec.a21190325_a21210392.cu_project;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
